48f6ce22790ff9bd4d1b7627a38d5884a8d72630
MatiasSalomon01 <matisalomond@gmail.com>
2025-03-22 19:07:23 -0300

    feat: colors added

---
+++ lib/constants/constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const white = Color(0xffffffff);
+const grey = Color(0xff6c6c6c);
+const lightGrey = Color(0xfff6f6f6);
+const black = Color(0xff040404);
+const lightGreen = Color(0xffe0ffe0);
+const lightPurple = Color(0xffe9e2ff);
+const lightRed = Color(0xfffce1d6);
+const lightBlue = Color(0xffb3cef9);
+const lightYellow = Color(0xfffff2b6);
+const lightTurquoise = Color(0xffb5fff0);
+const lightPink = Color(0xfffeadfc);
+const lightOrange = Color(0xffffdfa3);
